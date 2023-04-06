0361c380d58620c71cd4383e64885ea3b1603966
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-06 18:37:48 +0000

    7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- Creates a stored procedure ComputeAverageScoreForUser
+
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10,2);
+
+    -- Calculate the average score for the student
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+
+    -- Update the user's average score
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+END //
+DELIMITER ;
