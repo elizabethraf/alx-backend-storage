e5a84ee4d313de3841257cdfc00ad9a32b73b2f2
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-06 17:37:41 +0000

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Script that lists all bands with Glam rock
 -- Column names must be: band_name and lifespan (in years)
 
-SELECT band_name, IFNULL(split, 2023)-IFNULL(formed, 0) AS lifespan
+SELECT band_name, IFNULL(split, 2020)-IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
