5b5dc14f3c70bc99ae4059f8b5cb663ef84de1f5
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-04-11 11:22:31 -0700

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,7 +5,7 @@
 def list_all(mongo_collection):
     """Return an empty list if no document in the collection"""
     documents = mongo_collection.find()
-    
+
     if documents.count() == 0:
         return []
 
